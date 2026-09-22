66d075e83dc1ca3abcb588973a47e3577f6fe48c
Jorge-Tropigas <jorge.grullon@tomzagroup.com>
2026-09-22 13:16:55 -0600

    android: update namespace, Java/Kotlin version, and compileSdk
    - Change namespace from com.example to com.grullondev
    - Upgrade Java/Kotlin target to version 21
    - Pin compileSdk to 36 for connectivity_plus AndroidX deps
    - Update app label to 'NegocioListo Pro'
    - Add subproject namespace patch for legacy plugins

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -5,26 +5,25 @@ plugins {
 }
 
 android {
-    namespace = "com.example.negocio_listo_pro"
-    compileSdk = flutter.compileSdkVersion
+    namespace = "com.grullondev.negocio_listo_pro"
+    // connectivity_plus's AndroidX deps (fragment 1.7.1, window 1.2.0, etc.)
+    // require compileSdk 34+; flutter.compileSdkVersion still resolves to 33
+    // on this Flutter version, so it's pinned here until the SDK catches up.
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.negocio_listo_pro"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
+        applicationId = "com.grullondev.negocio_listo_pro"
+
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        // Uses the version code from pubspec.yaml. When using split APKs, 1000 * ABI_VERSION
-        // is added automatically by Flutter. (https://developer.android.com/studio/build/configure-apk-splits#configure-APK-versions)
-        // You can force using the value of versionCode by specifying the `-P force-version-code-ignoring-abi=true`
-        // flag during build.
+
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
@@ -40,7 +39,7 @@ android {
 
 kotlin {
     compilerOptions {
-        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17
+        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21
     }
 }
 
